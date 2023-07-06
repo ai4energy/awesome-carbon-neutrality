b62acdce4f02f2faf93f06ad9185fbf0eb121a8e
Documenter.jl <documenter@juliadocs.github.io>
2023-07-06 14:22:24 +0000

    build based on 7f2864b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"SolarEnergy/1-SAM/#SAM-介绍","page":"SAM 介绍","title":"SAM 介绍","text":"","category":"section"},{"location":"#碳中和开源软件汇编","page":"Home","title":"碳中和开源软件汇编","text":"","category":"section"},{"location":"#太阳能","page":"Home","title":"太阳能","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Pages = map(file -> joinpath(\"SolarEnergy\", file), readdir(\"SolarEnergy\"))","category":"page"}]
+}
